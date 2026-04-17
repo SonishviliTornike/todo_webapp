537649d60c930a4d567dd839c963519f852a7e87
 SonishviliTorni <sonishvili.tornike@gmail.com>
2026-04-17 23:09:08 +0400

    added: passwrod validation, hashing

--- src/Validation/UserValidation.php
+++ src/Validation/UserValidation.php
@@ -9,6 +9,8 @@ class UserValidation {
 
     public function processUserRegister(){
         $this->proccessFlow();
+
+        return [$this->data, $this->errors];
     }
 
 
@@ -18,6 +20,10 @@ class UserValidation {
         $this->processUserName();
 
         $this->processEmail();
+
+        $this->processFullName();
+
+        $this->processPassword();
     }
 
 
@@ -27,13 +33,19 @@ class UserValidation {
         if ($id === '') {
             return;
         }
+        
+        if (!ctype_digit($id)) {
+           $this->errors['id'][] = 'User information can\'t be updated:string detected.'; 
+           return;
+        }
 
-        if((int)$id <= 0 || !ctype_digit($id)) {
+        if((int)$id <= 0) {
             $this->errors['id'][] = 'User information can\'t be updated:Invalid id.';
             return;
-        } else {
-            $this->data['id'] = (int)$id;
         }
+
+        $this->data['id'] = (int)$id;
+        
     }
 
 
@@ -41,7 +53,7 @@ class UserValidation {
         $userName = trim($this->input['userName'] ?? '');
 
         
-        if (empty($userName) || strlen($userName) > 55  || strlen($userName < 3)) {
+        if (empty($userName) || strlen($userName) > 55  || strlen($userName) < 3) {
             $this->errors['userName'][] = 'User name must be min 3 characters long and max  55 characters long';
             return;
         }
@@ -69,9 +81,8 @@ class UserValidation {
         
         $splittedEmail = explode('@', $email);
         $hostname = strtolower($splittedEmail[1]);
-        $mxhosts = [];
 
-        if (!getmxrr($hostname, $mxhosts)) {
+        if (!dns_get_record($hostname)) {
             $this->errors['email'][] = 'Invalid email domain address.';
             return;
         }
@@ -80,7 +91,7 @@ class UserValidation {
     }
 
     private function processFullName() {
-        $fullName = ($this->input['fullName'] ?? '');
+        $fullName = trim($this->input['fullName'] ?? '');
         
         if(strlen($fullName) > 100 || strlen($fullName) < 3 ) {
             $this->errors['fullName'][] = 'Full name can\'t be more than 100 characters or less than 3.';
@@ -89,5 +100,26 @@ class UserValidation {
 
         $this->data['fullName'] = $fullName;
     }
+
+    private function processPassword() {
+        $password = trim($this->input['password'] ?? '');
+        
+
+        if($password === '') {
+            $this->errors['password'][] = 'Password cant be blank.';
+            return;
+        }
+
+        if(strlen($password) <= 10) {
+            $this->errors['password'][] = 'Password must be minimum 11 characters long.';
+            return;
+        }
+
+       $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+
+       $this->data['passwordHash'] = $hashedPassword;
+
+    }
+
 
 }
